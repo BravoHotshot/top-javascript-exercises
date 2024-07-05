3bd285a2ac7d7e491288db886b303fed06d53f8e
Bravo Hotshot <98163699+BravoHotshot@users.noreply.github.com>
2024-07-05 23:46:12 +0530

    Complete Ex4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    for (const elementToRemove of arguments) {
+        let index = 0;
+        let arrayLength = array.length;
+
+        while (index < arrayLength) {
+            if (array[index] === elementToRemove) {
+                array.splice(index, 1);
+                arrayLength--;
+            }
+
+            else {
+                index++;
+            }
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
