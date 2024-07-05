99f2f7a74d736dd58d06b6b307aa325de7dcc0c3
Bravo Hotshot <98163699+BravoHotshot@users.noreply.github.com>
2024-07-06 00:07:04 +0530

    Complete Ex5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(numberOne, numberTwo) {
+    if (Number.isInteger(numberOne) && Number.isInteger(numberTwo) && numberOne >= 0 && numberTwo >= 0) {
+        let sum = 0;
+        const startNumber = Math.min(numberOne, numberTwo);
+        const endNumber = Math.max(numberOne, numberTwo);
 
+        for (let number = startNumber; number <= endNumber; number++) {
+            sum += number;
+        }
+
+        return sum;
+    }
+
+    else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
