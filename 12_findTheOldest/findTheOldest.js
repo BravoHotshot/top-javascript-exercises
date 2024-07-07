0c6e0776719163a2834efdace69730a0465cf4f7
Bravo Hotshot <98163699+BravoHotshot@users.noreply.github.com>
2024-07-07 17:37:03 +0530

    Complete Ex12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(arrayOfPeople) {
+    arrayOfPeople.map(person => {
+        if (!("yearOfDeath" in person)) {
+            const currentDate = (new Date()).getFullYear();
+            person.age = currentDate - person.yearOfBirth;
+        }
 
+        else {
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        }
+    });
+
+    const maxAge = arrayOfPeople
+    .reduce((maxAge, currentPerson) => Math.max(maxAge, currentPerson.age), 0);
+
+    return arrayOfPeople.filter(person => person.age == maxAge)[0];
 };
 
 // Do not edit below this line
