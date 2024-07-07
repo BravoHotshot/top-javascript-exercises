fde00b8fd8a0f1c836ae043d2998a280ca559078
Bravo Hotshot <98163699+BravoHotshot@users.noreply.github.com>
2024-07-07 17:06:35 +0530

    Complete Ex10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function(finalIndex) {
+    let firstValue = 0;
+    let secondValue = 1;
+    let currentValue;
 
+    if (finalIndex > 0) {
+        for (let currentIndex = 1; currentIndex <= finalIndex; currentIndex++) {
+            if (currentIndex == 1) {
+                currentValue = 1;
+            }
+
+            else {
+                currentValue = firstValue + secondValue;
+                firstValue = secondValue;
+                secondValue = currentValue;
+            }
+        }
+        return currentValue;
+    }
+
+    else if (finalIndex == 0) {
+        return 0;
+    }
+
+    else {
+        return "OOPS";
+    }
 };
 
 // Do not edit below this line
