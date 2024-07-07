a20fe2882e3b717d80cc6858986c4f8d7bf2b51a
Bravo Hotshot <98163699+BravoHotshot@users.noreply.github.com>
2024-07-07 16:53:41 +0530

    Complete Ex9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const array = string.toLowerCase().split("");
+    const alphanumeric = "abcdefghijklmnopqrstuvwxyz0123456789";
+    const cleanArray = array.filter(character => alphanumeric.includes(character));
+    subArray1 = cleanArray.slice(0, Math.floor(cleanArray.length / 2));
+    subArray2 = cleanArray.slice(Math.ceil(cleanArray.length / 2)).reverse();
+    return subArray1.join("") == subArray2.join("");
 };
 
 // Do not edit below this line
